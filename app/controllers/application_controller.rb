5c5fa2bb8c08a22f524a760d7025f2fac06083e0
nakao keisuke <r3grashopper193@gmail.com>
2022-04-21 21:05:10 +0900

    fix user layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.production?
+
+  def set_layout
+    if params[:controller].match(%r{\A(fp|user)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
+  end
+
+  private :set_layout
 end

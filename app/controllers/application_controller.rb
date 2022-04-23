c482bb28be2f3a8573a01824b8a436e57256f9e4
nakao keisuke <r3grashopper193@gmail.com>
2022-04-23 23:44:44 +0900

    add error handle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  class Forbidden < ActionController::ActionControllerError; end
-  class IpAddressRejected < ActionController::ActionControllerError; end
-
   include ErrorHandlers if Rails.env.production?
 
   def set_layout
     if params[:controller].match(%r{\A(fp|user)/})
       Regexp.last_match[1]
     else
-      'customer'
+      'user'
     end
   end
 
---
+++ app/controllers/concerns/error_handlers.rb
@@ -0,0 +1,23 @@
+module ErrorHandlers
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from StandardError, with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+    rescue_from ActionController::ParameterMissing, with: :rescue400
+  end
+
+  private
+
+  def rescue500(_err)
+    render 'errors/internal_server_error', status: :internal_server_error
+  end
+
+  def rescue404(_err)
+    render 'errors/not_found', status: :not_found
+  end
+
+  def rescue400(_err)
+    render 'errors/bad_request', status: :bad_request
+  end
+end
